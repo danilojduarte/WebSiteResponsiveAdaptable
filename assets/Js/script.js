475971cf6cc7210cdfdb31ec0e03de0781ccfed3
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-09-10 04:15:09 -0300

    Removendo conteúdo javascript

--- assets/Js/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.select-btn').forEach(button => {
-  button.addEventListener('click', (event) => {
-      const selectedTheme = event.target.getAttribute('data-theme');
-      alert(`Você selecionou o ${selectedTheme}`);
-      // Aqui você pode adicionar qualquer lógica para lidar com o tema escolhido.
-  });
-});
